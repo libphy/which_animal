845e264464bcffba6de7cf6b2fb432db59360985
Geena <libphy@gmail.com>
2016-07-13 03:22:59 -0600

    cat scraped

--- sound_scrap.py
+++ sound_scrap.py
@@ -1,25 +1,58 @@
 from bs4 import BeautifulSoup as BS
 import requests
-import dryscrape
+import cPickle as pickle
 
 mainpage = 'http://www.tierstimmenarchiv.de/webinterface/guest.php'
 s = requests.Session()
-
 res = s.get(mainpage)
 cookies = dict(res.cookies)
-url = 'http://www.tierstimmenarchiv.de/webinterface/contents/querytext.php?mode=clearresults&querytext_1=Felis%20silvestris%20f.%20domestica&queryfield_1=species&querytype_1=matches&fields=1&startvalue=1'
-r=s.get(url,cookies=cookies)
-soup = BS(r.text,'html')
-datapglink = ['http://www.tierstimmenarchiv.de/webinterface/contents/'+x.find('a')['href'] for x in soup.findAll('tr',attrs={'class':"oddrow"})+soup.findAll('tr',attrs={'class':"evenrow"})]
-# r2 = s.get(datapglink[0], cookies= cookies)
-# soup2 = BS(r2.content,'html')
-# soup2.findAll('form', attrs={'action': "http://www.tierstimmenarchiv.de/filedownload.php"})
 
 
+def get_pages(url, lastpg): #returns list of item pages
+    pages=[]
+    for i in range(int(lastpg)):
+        pgurl = url[:-1]+str(i+1)
+        r_i=s.get(pgurl,cookies=cookies)
+        soup_i = BS(r_i.text,'html')
+        pages += ['http://www.tierstimmenarchiv.de/webinterface/contents/'+x.find('a')['href'] for x in soup_i.findAll('tr',attrs={'class':"oddrow"})+soup_i.findAll('tr',attrs={'class':"evenrow"})]
+    return pages
 
-import dryscrape
-sess = dryscrape.Session()
-sess.visit(datapglink[0])
-response = sess.body()
-soup = BeautifulSoup(response)
-soup.findAll('form')
+def get_download_links(pglinks, s, cookies): #extracts a download link from an item page
+    links=[]
+    errors=[]
+    for i in range(len(pglinks)):
+        r2 = s.get(pglinks[i], cookies= cookies)
+        soup2 = BS(r2.content,'xml')
+        try:
+            fname = soup2.findAll('div',attrs={'class':"toolbar"})[0].findAll('form')[0].findAll('form',attrs={'method':"POST",'target':"_blank"})[0]['action']
+            links.append('http://www.tierstimmenarchiv.de/webinterface/contents/'+fname)
+            print 'item '+str(i+1)+' OK'
+        except IndexError:
+            errors.append(i)
+            print 'item '+str(i+1)+' Fail'
+    return links, errors
+
+def datalinks_species(url):
+    r0=s.get(url,cookies=cookies)
+    soup0 = BS(r0.text,'html')
+    lastpg = soup0.findAll('table')[-1].findAll('input')[-1]['value']
+    datapglinks = get_pages(url,lastpg)
+    print len(datapglinks), ' files'
+    batch = datapglinks
+    failedbatch=[]
+    downloadlinks=[]
+    i=0
+    while i<5 and len(failedbatch)<len(datapglinks):
+        print 'iteration: ', i
+        links_success, failed_index = get_download_links(batch, s, cookies)
+        failedbatch = map(lambda i: batch[i],failed_index)
+        downloadlinks += links_success
+        batch = failedbatch
+        i+=1
+    print len(downloadlinks), ' success from ', len(datapglinks), ' results'
+    return downloadlinks
+
+if __name__ == '__main__':
+    cat_url = 'http://www.tierstimmenarchiv.de/webinterface/contents/querytext.php?mode=clearresults&querytext_1=Felis%20silvestris%20f.%20domestica&queryfield_1=species&querytype_1=matches&fields=1&startvalue=1'
+    with open( "catlinks.pkl", "w" ) as f:
+        pickle.dump(downloadlinks, f)
